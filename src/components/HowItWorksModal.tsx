5407afea7b1f82a6abfb2e548baa1fcf9bdd0d83
Umar Shafi <101823809+Umar1Shafi@users.noreply.github.com>
2025-09-01 19:59:33 +0530

    Delete src directory

--- src/components/HowItWorksModal.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
-
-export const HowItWorksModal = () => {
-  // This component is now integrated into the Header component
-  // Keeping this file for potential future use
-  return null;
-};
